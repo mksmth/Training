a7cf2965968c24dfce383dac8cce8f8805c8470a
Mike Smith <mk.smth@gmail.com>
2019-05-17 16:29:55 +0100

    Given solutions added to files; cleaner than my maths :/

--- eloquentJavascript/js/ch02-fizzBuzz.js
+++ eloquentJavascript/js/ch02-fizzBuzz.js
@@ -5,11 +5,21 @@ for(i=1; i<=100; i++) {
   if (i % 3 == 0 && i % 5 == 0){
     console.log("FizzBuzz");
   }
-    else  if (i % 3 == 0) {
+  else  if (i % 3 == 0) {
     console.log("Fizz");
   }
   else if (i % 5 == 0) {
     console.log("Buzz");
   }
+}
 
+//Given Solution:
+
+for (var n = 1; n <= 100; n++) {
+  var output = "";
+  if (n % 3 == 0)
+    output += "Fizz";
+  if (n % 5 == 0)
+    output += "Buzz";
+  console.log(output || n);
 }
--- eloquentJavascript/js/ch02-hashTriangle.js
+++ eloquentJavascript/js/ch02-hashTriangle.js
@@ -3,3 +3,6 @@ for(i=0; i<=6; i++) {
   console.log(hash);
   hash += "#";
 }
+
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
